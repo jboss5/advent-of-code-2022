a5a478c935afe2edbadecad4fbfce5e984b39c22
Jordan <>
2022-12-04 15:45:13 -0500

    made it better + timing

--- dec-01-rust/src/main.rs
+++ dec-01-rust/src/main.rs
@@ -1,23 +1,21 @@
 use std::fs::File;
-use std::io::{prelude::*, BufReader};
+use std::io::{BufRead, BufReader};
+use std::time::Instant;
 
 fn main() {
+    let now = Instant::now();
     let file = File::open("input.txt").expect("Error opening input file");
     let reader = BufReader::new(file);
-    let mut calories: Vec<i32> = Vec::new();
+    let mut calories = vec![0];
     let mut index = 0;
-    calories.push(0);
 
-    for l in reader.lines() {
-        let line = &l.unwrap();
-        if line != "" {
-            match line.parse::<i32>() {
-                Ok(n) => calories[index] = n + calories[index],
-                Err(e) => eprintln!("found non-number in input: {}", e),
+    for line in reader.lines() {
+        match line.unwrap().parse::<i32>() {
+            Ok(num) => calories[index] = num + calories[index],
+            Err(_) => {
+                index += 1;
+                calories.push(0);
             }
-        } else {
-            index += 1;
-            calories.push(0);
         }
     }
 
@@ -25,4 +23,5 @@ fn main() {
     let top3sum = calories[0] + calories[1] + calories[2];
     println!("highest calorie count: {}", calories[0]);
     println!("top 3 sum: {}", top3sum);
+    println!("time elapsed: {:.2?}", now.elapsed());
 }
